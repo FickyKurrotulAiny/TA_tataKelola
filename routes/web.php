d263595293ce1034932c139db5c40d257ea6ffdf
BocahGabut <randkill.leader@gmail.com>
2023-05-24 21:05:26 +0700

    commit another account

--- routes/web.php
+++ routes/web.php
@@ -1,70 +1,33 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PinjamController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\InventarisController;
-use App\Http\Controllers\PeminjamanController;
-use App\Http\Controllers\PersediaanController;
 
+use App\Http\Controllers;
 
+date_default_timezone_set('Asia/Jakarta');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/login', [Controllers\LoginController::class, 'login'])->name('login');
+Route::post('/postlogin', [Controllers\LoginController::class, 'postlogin'])->name('postlogin');
+Route::get('/logout', [Controllers\LoginController::class, 'logout'])->name('logout');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/', [Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/user', Controllers\UserController::class);
+    Route::get('getUser', [Controllers\UserController::class, 'getUser'])->name('getUser');
+    // Route::post('/login', [Controllers\LoginController::class, 'authenticate'])->name('login1');
+    // Route::get('/logout', [Controllers\LoginController::class, 'actionlogout'])->name('logout');
 
-// Route::get('/home', function () {
-//     return view('home');
-// });
+    //dasboard
+    Route::get('getDashboard', [Controllers\DashboardController::class, 'getDashboard'])->name('getDashboard');
 
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/postlogin', [LoginController::class, 'postlogin'])->name('postlogin');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+    //peminjaman
+    Route::resource('/peminjaman', Controllers\PeminjamanController::class);
+    Route::get('getPeminjaman', [Controllers\PeminjamanController::class, 'getPeminjaman'])->name('getPeminjaman');
 
-// user
-Route::resource('/user', UserController::class);
-Route::get('getUser', [UserController::class, 'getUser'])->name('getUser');
-// Route::post('/login', [LoginController::class, 'authenticate'])->name('login1');
-// Route::get('/logout', [LoginController::class, 'actionlogout'])->name('logout');
+    //barang.inventaris
+    Route::resource('/inventaris', Controllers\InventarisController::class);
+    Route::get('getInventaris', [Controllers\InventarisController::class, 'getInventaris'])->name('getInventaris');
 
-// Route::group(['middleware' => ['auth']], function (){
-
-// });
-
-// Route::get('/dashboard', function (){
-//     return view('index');
-// });
-
-//dasboard
-// Route::get('getDashboard', [DashboardController::class, 'getDashboard'])->name('getDashboard');
-
-//peminjaman
-Route::resource('/peminjaman', PeminjamanController::class);
-Route::get('getPeminjaman', [PeminjamanController::class, 'getPeminjaman'])->name('getPeminjaman');
-
-//barang.inventaris
-Route::resource('/inventaris', InventarisController::class);
-Route::get('getInventaris', [InventarisController::class, 'getInventaris'])->name('getInventaris');
-
-//barang.persediaan
-Route::resource('/persediaan', PersediaanController::class);
-Route::get('getPersediaan', [PersediaanController::class, 'getPersediaan'])->name('getPersediaan');
-
-//pinjam
-Route::resource('/pinjam', PinjamController::class);
-Route::get('getPinjam', [PinjamController::class, 'getPinjam'])->name('getPinjam');
-Route::get('generatePDFPinjam/{$id}', [PinjamController::class, 'generatePDFPinjam'])->name('generatePDFPinjam');
+    //barang.persediaan
+    Route::resource('/persediaan', Controllers\PersediaanController::class);
+    Route::get('getPersediaan', [Controllers\PersediaanController::class, 'getPersediaan'])->name('getPersediaan');
+});
